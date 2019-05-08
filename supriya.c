31c66ebb9d5b4029ff4d342c126b3cc5e00707e3
gayathrisai96 <50397493+gayathrisai96@users.noreply.github.com>
2019-05-08 16:15:04 +0530

    Update supriya.c

--- supriya.c
+++ supriya.c
@@ -2,10 +2,10 @@
 int main()
 {
     int i, j;
-    char input, alphabet = 'A';
+    char alphabet = 'A';
 
     
-    for(i=1; i <= (5-'A'+1); ++i)
+    for(i=1; i <= ('E'-'A'+1); ++i)
     {
         for(j=1;j<=i;++j)
         {
