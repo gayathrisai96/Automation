6097423f9bb6e17c6f3f2beb771e8020efafd0ca
supriya <50396195+supriyakatam@users.noreply.github.com>
2019-05-08 15:44:09 +0530

    Update supriya.c

--- supriya.c
+++ supriya.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int firstNumber=10,secondNumber=20;
+    int sumOfTwoNumbers;
 
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    // Displays sum      
+    printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
+
+    return 0;
+}
